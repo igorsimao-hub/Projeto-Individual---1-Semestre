b1a0510c1dd5d22f59f2c5fd2e3e2b4442c7d573
igorsimao-hub <igor.simao@sptech.school>
2025-05-15 09:32:42 -0300

    arrumando problemas de commit

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,8 +1,4 @@
 var usuarioModel = require("../models/usuarioModel");
-<<<<<<< HEAD
-=======
-var aquarioModel = require("../models/aquarioModel");
->>>>>>> 4bf4c4122617eb72c7339769d66de070f40bebb8
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -20,7 +16,6 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-<<<<<<< HEAD
                     if (resultadoAutenticar.length == 1) { //MUDEI AQUI
                         console.log(resultadoAutenticar);
                                     res.json({
@@ -30,25 +25,6 @@ function autenticar(req, res) {
                                         //senha: resultadoAutenticar[0].senha (COMENTEI POR QUESTÕES DE SEGURANÇA)
                                     });
 
-=======
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
->>>>>>> 4bf4c4122617eb72c7339769d66de070f40bebb8
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -71,11 +47,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-<<<<<<< HEAD
     //MUDEI AQUI
-=======
-    var fkEmpresa = req.body.idEmpresaVincularServer;
->>>>>>> 4bf4c4122617eb72c7339769d66de070f40bebb8
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -84,19 +56,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-<<<<<<< HEAD
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha)
-=======
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
->>>>>>> 4bf4c4122617eb72c7339769d66de070f40bebb8
             .then(
                 function (resultado) {
                     res.json(resultado);
