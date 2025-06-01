dd4be09d128540720a0f6ba7d899593fdbbac5fa
igorsimao-hub <igor.simao@sptech.school>
2025-06-01 16:05:18 -0300

    adicionando select na dashboard

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
+
 
 var avaliacaoRouter = require("./src/routes/avaliacao"); // criando a rota de avaliação
 
@@ -32,11 +29,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
-
 app.use("/avaliacao", avaliacaoRouter); // usando a rota de avaliação
 
 
